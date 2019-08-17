41160ad99318d9c63ba204fcb52ba781bc05f04d
cjonesrun3 <31106248+cjonesrun3@users.noreply.github.com>
2019-08-17 14:32:34 -0500

    Added more scripts

---
+++ RetrieveTableNamesFromMicrosoftAccessDB.py
@@ -0,0 +1,32 @@
+import pyodbc
+from openpyxl import Workbook
+from openpyxl.styles import colors
+from openpyxl.styles import Font
+
+data = []  # HOLDS LISTS INSTEAD OF pyobdc.rows DATA TYPE.
+
+#  BELOW SPECIFIES LOCATION OF TARGET DB, DRIVER, AND PASSWORD IF NECESSARY
+data_target = 'Path to .mdb'; DRV = '{Microsoft Access Driver (*.mdb)}'; PWD = ''
+
+# LIKE SQLLITE THIS CONNECTS UP TO THE DATABASE
+conn = pyodbc.connect('DRIVER={};DBQ={};PWD={}'.format(DRV, data_target, PWD))
+
+cur = conn.cursor()
+
+for row in cur.tables():  # QUERIES ALL TABLE NAMES
+    print('AVAILABLE TABLE NAME: ' + str(row.table_name))
+
+for row in cur.columns(table='my_db'):  # GETS COLUMN NAMES FROM SPECIFIC TABLE
+    print('COLUMN HEADER: ' + str(row.column_name))
+
+sql = 'SELECT * FROM my_db'  # SELECTS ALL DATA FROM THIS TABLE
+
+rows = cur.execute(sql).fetchall()
+for item in rows:
+    data.append(list(item))  # CONVERTS pyodbc.rows FORMAT INTO A LIST AND SENDS IT TO THE DATA LIST
+
+cur.close()
+conn.close()
+
+for item in data:
+    print(item)
